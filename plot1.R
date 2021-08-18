332cb66c5102857fa3262fbde246cdc003d5705f
psmanta <69612179+psmanta@users.noreply.github.com>
2021-08-17 21:21:27 -0700

    plot1
    Initial Commit of data plot1 code

---
+++ plot1.R
@@ -0,0 +1,14 @@
+# plot1.R
+
+#Read data and subset for only Feb 1 & Feb 2, 2007
+setwd("C:/GitHub/GettingCleaningData/ProgrammingAssignment/ExData_Plotting1")
+data <- read.table("household_power_consumption.txt", header = TRUE, sep=";")
+plotData <- subset(data, data$Date=="1/2/2007" | data$Date=="2/2/2007")
+#Clear up memory..
+rm(data)
+
+# concatenate date & time for strptime and convert character strings
+datetime <- paste(plotData$Date, plotData$Time, sep=" ")
+plotData$Time <- strptime(datetime, format = "%d/%m/%Y %H:%M:%S")
+rm(datetime)
+plotData$Date <- as.Date(plotData$Date)
