aacd6ac7997010729186775f927648825996e58d
psmanta <69612179+psmanta@users.noreply.github.com>
2021-08-24 20:18:22 -0700

    Plot1.png
    Completed plot1

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 
 #Read data and subset for only Feb 1 & Feb 2, 2007
 setwd("C:/GitHub/GettingCleaningData/ProgrammingAssignment/ExData_Plotting1")
-data <- read.table("household_power_consumption.txt", header = TRUE, sep=";")
+data <- read.table("household_power_consumption.txt", header = TRUE, sep=";", na.strings="?")
 plotData <- subset(data, data$Date=="1/2/2007" | data$Date=="2/2/2007")
 #Clear up memory..
 rm(data)
@@ -12,3 +12,7 @@ datetime <- paste(plotData$Date, plotData$Time, sep=" ")
 plotData$Time <- strptime(datetime, format = "%d/%m/%Y %H:%M:%S")
 rm(datetime)
 plotData$Date <- as.Date(plotData$Date, format = "%d/%m/%Y")
+
+png(filename="plot1.png", width=480, height=480)
+hist(plotData$Global_active_power, main="Global Active Power", xlab="Global Active Power (Kilowatts)", col="Red")
+dev.off()
